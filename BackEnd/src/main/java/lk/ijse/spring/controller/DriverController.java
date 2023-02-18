f27231e6e48e59df16e1b9acca9559ef02d19006
unknown <sumeraruwen@gmail.com>
2023-02-18 16:35:06 +0530

    content updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -4,14 +4,37 @@ import lk.ijse.spring.dto.DriverDTO;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/driver")
 public class DriverController {
 
     @PostMapping
-    public ResponseUtil saveDrivers(DriverDTO dto){
+    public ResponseUtil saveDrivers(@ModelAttribute DriverDTO dto){
         System.out.println(dto.toString());
         return new ResponseUtil("ok","Successsfully saved",null);
+    }
+
+    @DeleteMapping(params = {"dId"})
+    public ResponseUtil deleteDriver(@RequestParam String dId){
+        return new ResponseUtil("ok","deleted "+dId,null);
+    }
+
+    @PutMapping
+    public ResponseUtil updateDriver(@RequestBody DriverDTO dto){
+        return new ResponseUtil("ok","scuccess Updated ",null);
+    }
+
+    @GetMapping
+    public ResponseUtil getAllDrivers(){
+        ArrayList<DriverDTO> arrayList= new ArrayList<>();
+        arrayList.add(new DriverDTO("D001","sup","colombo","sff","sdfsdf",45,"dfs","sdf"));
+        arrayList.add(new DriverDTO("D002","vik","colombo","sff","sdfsdf",45,"dfs","sdf"));
+        arrayList.add(new DriverDTO("D003","sum","colombo","sff","sdfsdf",45,"dfs","sdf"));
+        arrayList.add(new DriverDTO("D004","ven","colombo","sff","sdfsdf",45,"dfs","sdf"));
+        arrayList.add(new DriverDTO("D005","ruw","colombo","sff","sdfsdf",45,"dfs","sdf"));
+        return new ResponseUtil("ok","scuccess loaded",arrayList);
     }
 }
