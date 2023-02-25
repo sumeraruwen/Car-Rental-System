6e4da5279dbd5a8cdc3241ae8669ee53bf271da7
unknown <sumeraruwen@gmail.com>
2023-02-25 16:50:34 +0530

    update

--- BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -4,17 +4,21 @@ package lk.ijse.spring.config;
 import lk.ijse.spring.advisor.AppWideExceptionHandler;
 import lk.ijse.spring.controller.CustomerController;
 import lk.ijse.spring.controller.DriverController;
+import lk.ijse.spring.controller.VehicleController;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {DriverController.class, CustomerController.class, AppWideExceptionHandler.class})
-public class WebAppConfig {
-
-
+@ComponentScan(basePackageClasses = {DriverController.class, CustomerController.class, VehicleController.class, AppWideExceptionHandler.class})
+public class WebAppConfig implements WebMvcConfigurer {
 
 }
